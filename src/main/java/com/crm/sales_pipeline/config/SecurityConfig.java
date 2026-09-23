ecd542eb191a1f6a69417b8b1129febd4b038dd3
tg968258-glitch <tg968258@gmail.com>
2026-09-23 08:19:24 +0530

    Enabled security configuration

--- src/main/java/com/crm/sales_pipeline/config/SecurityConfig.java
+++ src/main/java/com/crm/sales_pipeline/config/SecurityConfig.java
@@ -4,12 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request ->
+                        request.anyRequest().authenticated())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
         return new JwtAuthenticationFilter();
@@ -26,5 +44,6 @@ public class SecurityConfig {
 
         return authConfig.getAuthenticationManager();
     }
+
 }
 
